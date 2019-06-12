3c8b9361c87cfd922a2d642389db9e355f57b0d2
sakurahu <lili_sakura@126.com>
2019-06-12 11:47:58 +0800

    update

--- build/webpack.client.conf.js
+++ build/webpack.client.conf.js
@@ -3,21 +3,31 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
 const vueLoaderConfig = require('./vue-loader.conf')
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const clientWebpackConfig = merge(baseWebpackConfig, {
   entry: {
     app: './src/entry-client.js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minChunks: Infinity,
+      automaticNameDelimiter: '-',
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_nodules[\\/]/,
+          priority: -10,
+        },
+      },
+    },
+    runtimeChunk: 'single',
   },
   plugins: [
     // strip dev-only code in Vue source
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
       'process.env.VUE_ENV': '"client"'
-    }),
-    // extract vendor chunks for better caching
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "manifest",
-      minChunks: Infinity
     }),
     // 此插件在输出目录中
     // 生成 `vue-ssr-client-manifest.json`。
@@ -27,13 +37,20 @@ const clientWebpackConfig = merge(baseWebpackConfig, {
 
 if (process.env.NODE_ENV === 'production') {
   clientWebpackConfig.plugins.push(
-    // minify JS
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        drop_console: true
-      }
-    })
+    new UglifyJSPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+        compress: {
+          warnings: false,
+          drop_console: true,
+        },
+      },
+    }),
   )
 }
 
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -24,16 +25,35 @@ const webpackConfig = merge(baseWebpackConfig, {
     filename: utils.assetsPath('scripts/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('scripts/[name].[id].[chunkhash].js')
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minChunks: Infinity,
+      automaticNameDelimiter: '-',
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_nodules[\\/]/,
+          priority: -10,
+        },
+      },
+    },
+    runtimeChunk: 'single',
+  },
   plugins: [
     // UglifyJs do not support ES6+, you can also use babel-minify for better treeshaking: https://github.com/babel/minify
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
+    new UglifyJSPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+        compress: {
+          warnings: false,
+          drop_console: true,
+        },
       },
-      mangle: {
-        safari10: true // 解决iOS 10 白屏问题 参考链接：https://mp.weixin.qq.com/s/DO5wHuDIqNK7tGTLwGEHpQ
-      },
-      sourceMap: true
     }),
     // extract css into its own file
     new ExtractTextPlugin({
@@ -62,26 +82,6 @@ const webpackConfig = merge(baseWebpackConfig, {
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency'
-    }),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
